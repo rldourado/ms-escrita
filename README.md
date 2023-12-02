a97ffb099548e8161fcdd63b9a76b1d16775f390
Rubens Levy Dourado <rubens.dourado@gmail.com>
2023-12-02 23:15:24 +0000

    App

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ms-escrita
+
+
+## To run RabbitMQ locally:
+
+```
+docker run -d --hostname rabbit --name rabbit -p 8080:15672 -p 5672:5672 -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password rabbitmq:3-management
+```
+
+## Kubernetes
+
+#### Pod Name Environment Variable
+
+
+```
+      env:
+        - name: APP_NAME
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.name
+```
---
+++ ms-escrita.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os
+import time
+
+import pika
+from dotenv import load_dotenv
+
+load_dotenv()
+
+amqp_url = os.getenv('AMQP_URL')
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+queue    = os.getenv('QUEUE')
+app_name = os.getenv('APP_NAME')
+message  = os.getenv('MESSAGE')
+timer    = os.getenv('TIMER')
+
+message = f'App: {app_name}\n{message}'
+
+timer = int(timer)
+
+connection = pika.BlockingConnection(
+  pika.ConnectionParameters(
+    host=amqp_url,
+    credentials=pika.PlainCredentials(username,password)
+  )
+)
+
+channel = connection.channel()
+
+channel.queue_declare(queue=queue)
+
+try:
+  while True:
+    time.sleep(timer)
+
+    channel.basic_publish(exchange='',
+                          routing_key=queue,
+                          body=message)
+
+    print(f"Message Sent:\n\n{message}\n")
+finally:
+  connection.close()
