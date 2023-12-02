bcbba52e36b5bade357b8ee752e825179c7feb82
Rubens Levy Dourado <rubens.dourado@gmail.com>
2023-12-02 23:51:32 +0000

    Update

--- ms-escrita.py
+++ ms-escrita.py
@@ -3,41 +3,41 @@
 import os
 import time
 
+import lorem
 import pika
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(verbose=True)
 
-amqp_url = os.getenv('AMQP_URL')
-username = os.getenv('USERNAME')
-password = os.getenv('PASSWORD')
-queue    = os.getenv('QUEUE')
-app_name = os.getenv('APP_NAME')
-message  = os.getenv('MESSAGE')
-timer    = os.getenv('TIMER')
+RABBITMQ_URL      = os.getenv('RABBITMQ_URL')
+RABBITMQ_USERNAME = os.getenv('RABBITMQ_USERNAME')
+RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+RABBITMQ_QUEUE    = os.getenv('RABBITMQ_QUEUE')
+APP_NAME          = os.getenv('APP_NAME')
+TIMER             = int(os.getenv('TIMER'))
 
-timer = int(timer)
+print(f"Sending message to queue {RABBITMQ_QUEUE} in {RABBITMQ_URL} every {TIMER} seconds\n")
 
 connection = pika.BlockingConnection(
   pika.ConnectionParameters(
-    host=amqp_url,
-    credentials=pika.PlainCredentials(username,password)
+    host=RABBITMQ_URL,
+    credentials=pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
   )
 )
 
 channel = connection.channel()
 
-channel.queue_declare(queue=queue)
+channel.queue_declare(queue=RABBITMQ_QUEUE)
 
 try:
   while True:
-    time.sleep(timer)
+    time.sleep(TIMER)
 
     ts = time.time()
-    message_body = f'App: {app_name}\nTimestamp: {ts}\nMessage: {message}'
+    message_body = f'App: {APP_NAME}\nTimestamp: {ts}\nMessage: {lorem.sentence()}'
 
     channel.basic_publish(exchange='',
-                          routing_key=queue,
+                          routing_key=RABBITMQ_QUEUE,
                           body=message_body)
 
     print(f"--------------------\nMessage Sent:\n\n{message_body}\n")
